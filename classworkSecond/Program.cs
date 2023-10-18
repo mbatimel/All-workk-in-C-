7815b9c09b1195c5c1c5e2fd704a5c8124a400d9
mbatimel <92187863+mbatimel@users.noreply.github.com>
2023-10-19 00:34:03 +0300

    Add files via upload

---
+++ classworkSecond/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace classworkSecond
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите число к");
+            string text = Console.ReadLine();
+            float k = Convert.ToSingle(text);
+            if (k > 1 && k < 0)
+            {
+                throw new Exception("к не принадлежит диапозону ");
+            }
+            float[] Number = Console.ReadLine().Split().Select(x => float.Parse(x)).ToArray();
+            {
+                for (int i = 0; i < Number.Length; i++)
+                {
+                    Number[i] = (float)Math.Sqrt(Number[i] * (i + 1) * k);
+                    Console.Write($"{Number[i]} ");
+                }
+            }
+            Console.ReadKey();
+        }
+        
+       
+    }
+}
