892bbe7e6f86e6e6b0be83cc4ea23ed4bf42c416
mbatimel <92187863+mbatimel@users.noreply.github.com>
2023-10-19 00:28:55 +0300

    Add files via upload

---
+++ эксперементы/эксперементы/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace эксперементы
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {    
+                int[] mas = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+                for (int i = 0; i < mas.Length; i++)
+                {
+                    Console.Write(mas[i]);
+                }
+
+            }
+        }
+    }
+}
+
