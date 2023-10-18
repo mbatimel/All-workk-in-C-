560512270b6fb03279a45ab5cc89c8a226dae31b
mbatimel <92187863+mbatimel@users.noreply.github.com>
2023-10-19 00:24:43 +0300

    Add files via upload

---
+++ массивы/массивы/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace массивы
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 1, 3, 4, 5, 7, 8, 9 };
+            foreach (int i in num)//позволяет сразу запихнуть в одну переменную все эелементы массива
+            { 
+                Console.WriteLine(i);
+            }
+            /*for(int i=0;i<num.Length ;i++)
+            {
+                Console.WriteLine(num[i]);
+            }можно использовать этот метод записи цикла */
+            /*int a = num.Length;
+           while(a>0) цикл вайл
+            {
+                Console.WriteLine(a);
+                
+                a--;
+                    }*/
+            Console.ReadKey();
+            
+        }
+    }
+}
