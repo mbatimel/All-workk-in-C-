713cdc26740f5ed308d219fe29722c15d900fd5f
mbatimel <92187863+mbatimel@users.noreply.github.com>
2023-10-18 23:57:28 +0300

    Add files via upload

---
+++ лаба1начало11/лаба1начало1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace лаба1начало1
+{
+    class Program
+    {//доделать часть,чтобы он выводил строчку значений массива
+
+        static int MassivVOd(int x,int y)
+        {
+           
+
+
+
+        }
+
+        
+    }
+    
+}
